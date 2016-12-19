c90a52769ab4fe28c6ce13f680814e8c2dd45241
jaypeeteeB <jaypeetee@tarakaja.de>
2016-12-19 15:47:16 +0100

    Changed parsing of scans to include dplanetscore

--- online/battlecalc/BCcode.php
+++ online/battlecalc/BCcode.php
@@ -903,29 +903,29 @@
 
 	function FillFleet( $Command, $Array )
 	{
-        global $ShipTypes, $Fleet, $ShipBattleRep, $PasteText;
+	        global $ShipTypes, $Fleet, $ShipBattleRep, $PasteText;
+	
+        	$Fleet[0]["Side"] = "<span class=attacker>Attacker</span>";
+	        $Fleet[1]["Side"] = "<span class=defender>Defender</span>";
+		$Fleet[0]["Totals"] = null;
+		$Fleet[1]["Totals"] = null;
 
-        $Fleet[0]["Side"] = "<span class=attacker>Attacker</span>";
-        $Fleet[1]["Side"] = "<span class=defender>Defender</span>";
-	$Fleet[0]["Totals"] = null;
-	$Fleet[1]["Totals"] = null;
+		if (ISSET( $Array["input"] )) {
+			/* Edit input value for netscape */
+			$Array["input"] = preg_replace( "/^\x20+(.*)$/m", "\\1", $Array["input"]);
+			$Array["input"] = preg_replace( "/\n\r/", "\n \r", $Array["input"]);
+			/* Edit  input value for opera & netscape finish */
+			$Array["input"] = preg_replace( "/\r\n/", " ", $Array["input"]);
 
-	if (ISSET( $Array["input"] )) {
-		/* Edit input value for netscape */
-		$Array["input"] = preg_replace( "/^\x20+(.*)$/m", "\\1", $Array["input"]);
-		$Array["input"] = preg_replace( "/\n\r/", "\n \r", $Array["input"]);
-		/* Edit  input value for opera & netscape finish */
-		$Array["input"] = preg_replace( "/\r\n/", " ", $Array["input"]);
+			$PasteText = $Array["input"];
+		}
 
-		$PasteText = $Array["input"];
-	}
-
-        /* process unit scan/overview/military pastes */
-	if ( ISSET($Array["input"]) && $Array["Addtype"] != "BattleReport" )
-	{
-		// $Array["input"] = preg_replace( "/\r/", " ", $Array["input"]);
-		// $Array["input"] = preg_replace( "/\n/", " ", $Array["input"]);
-		if ( ISSET($Array["fleetbase"]) || ISSET($Array["fleet1"]) || ISSET($Array["fleet2"]) || ISSET($Array["fleet3"]) )
+	        /* process unit scan/overview/military pastes */
+		if ( ISSET($Array["input"]) && $Array["Addtype"] != "BattleReport" )
+		{
+			// $Array["input"] = preg_replace( "/\r/", " ", $Array["input"]);
+			// $Array["input"] = preg_replace( "/\n/", " ", $Array["input"]);
+			if ( ISSET($Array["fleetbase"]) || ISSET($Array["fleet1"]) || ISSET($Array["fleet2"]) || ISSET($Array["fleet3"]) )
 				preg_match_all( "/(\w*\s?[\w]+)\s([0-9]+|\s)\s([0-9]+|\s)\s([0-9]+|\s)\s([0-9]+|\s)([^0-9a-zA-Z]|$)/iU", $Array["input"], $output, PREG_SET_ORDER  );
 			else
 				preg_match_all( "/(\w*\s?[\w]+)\s([0-9]+)([^0-9]|$)/isU", $Array["input"], $output, PREG_SET_ORDER  );
@@ -964,6 +964,11 @@
 					default : $ShipAddArray[$Array["Addtype"]][trim("$row[1]")] = $row[2];
 				}
 			}
+			/* seems to be missed */
+		        $Fleet[0]["PlanetScore"]      = (array_key_exists("aplanetscore",$Array)?$Array["aplanetscore"]:0);
+		        $Fleet[0]["PlanetScoreRatio"] = (array_key_exists("aplanetscoreratio", $Array)?$Array["aplanetscoreratio"]:0);
+		        $Fleet[1]["PlanetScore"]      = (array_key_exists("dplanetscore", $Array)?$Array["dplanetscore"]:0);
+		        $Fleet[1]["PlanetScoreRatio"] = (array_key_exists("dplanetscoreratio", $Array)?$Array["dplanetscoreratio"]:0);
 		}
 
 
